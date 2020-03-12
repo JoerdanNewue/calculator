938f2b8a61d9ea8c5d12b9869f3ad4854229d88a
Joerdan Newue <joerdannewue@gmail.com>
2020-03-12 22:17:15 +0300

    alternativniy variant

--- src/calculator.c
+++ src/calculator.c
@@ -11,26 +11,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void) {
-    float a,b,c,d,X;                           // 0-plus 1-minus 2-umnozhit' 3-delit' 4-fact 5-stepen'  X znachit deystvie
-    printf("vvedite a: ");
-    scanf("%f", &a);
-    printf("vvedite b: ");
-    scanf("%f", &a);
-    printf("vvedite simvol nuzhnogo deystvia");
-    scanf("%f", &X);
-    if (X==0) {c=a+b;}
-        else if (X==1)
-            {c=a-b;}
-        else if (X==2)                                                //RIADOVIE OPERACII BEZ OBYASNENIA
-                {c=a*b;}
-        else if (X==3)
-                {c=a*b;}
-        else if (X==4)
-                 {for (float i=a;     i>=0;   i=i-1)           // factor  chisel
-                {c=a*i;}}
-        else if (X==5)
-                {for (float i=0;i<=b;i++) {c=a*a;}}
-    printf("calculator podumal i vievel takoy otvet =%f\n",c);
+    int a,b,c;
+    char z;
+        printf ("%d\nVvedite a: ", a);
+        printf ("%d\nVvedite b: ", b);
+        printf ("%c\nVvedite znak deystvia ", z);
+    printf("a=%d", a);
+    printf("z=%c", z);
+    printf("b=%d", b);
+     scanf ("%d", &a);
+     scanf ("%d", &b);
+     scanf ("%c", &z);
+    if (z == "+") {
+        c = a+b;}
+    else if (z == "-") {
+        c = a-b;}
+    else if (z == "*") {
+        c = a*b;}
+    else if (z == "/") {
+        c = a/b;}
+    printf ("%d", c);
 }
-
-
